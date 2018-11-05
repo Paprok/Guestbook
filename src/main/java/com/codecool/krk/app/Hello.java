eda4d8fcbf2c6678c883df7a16f551c8f1f12871
Paprok <rafal.becla@gmail.com>
2018-11-05 19:03:53 +0100

    added pom.xml

---
+++ src/main/java/com/codecool/krk/app/Hello.java
@@ -0,0 +1,18 @@
+package com.codecool.krk.app;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Hello implements HttpHandler {
+
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String response = "Hello World!";
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
